226ab7bfa74720a269073007f410154fca490d93
Santiago Daleffe <santiagodaleffe@gmail.com>
2026-08-15 22:22:17 -0300

    .

--- api/utils/database.py
+++ api/utils/database.py
@@ -2,15 +2,23 @@ from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, Asyn
 import os
 
 DATABASE_URL = os.environ["DATABASE_URL"]
+
+engine_kwargs = {}
+if not DATABASE_URL.startswith("sqlite"):
+    engine_kwargs = {
+        "pool_size": 10,
+        "max_overflow": 20,
+        "pool_pre_ping": True,
+        "pool_recycle": 1800
+    }
+
 engine = create_async_engine(
     DATABASE_URL, 
     echo=False, 
     future=True,
-    pool_size=10,
-    max_overflow=20,
-    pool_pre_ping=True,
-    pool_recycle=1800
+    **engine_kwargs
 )
+
 AsyncSessionLocal = async_sessionmaker(
     bind=engine, 
     class_=AsyncSession, 
