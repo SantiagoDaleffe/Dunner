5c3b0753376497c68e66e6ccb973cefcdf39b546
Santiago Daleffe <santiagodaleffe@gmail.com>
2026-08-19 20:19:44 -0300

    .

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,71 +1,71 @@
 # 1. Create project
-resource "railway_project" "dunner_project" {
-  name        = "dunner-production"
-  description = "Motor de Dunning Serverless"
+resource "railway_project" "egida_project" {
+  name        = "egida"
+  description = "Fucina Egida - Dunning Engine"
 }
 
 # 2. Create service
-resource "railway_service" "dunner_api" {
-  project_id      = railway_project.dunner_project.id
-  name            = "dunner-api"
+resource "railway_service" "egida_api" {
+  project_id      = railway_project.egida_project.id
+  name            = "egida-api"
   source_repo     = var.github_repo
-  sleep_application = true
+  serverless = true
 }
 
 # 3. Inject environment variables
 resource "railway_variable" "db_url" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "DATABASE_URL"
   value          = var.database_url
 }
 
 resource "railway_variable" "qstash_token" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "QSTASH_TOKEN"
   value          = var.qstash_token
 }
 
 resource "railway_variable" "qstash_curr_key" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "QSTASH_CURRENT_SIGNING_KEY"
   value          = var.qstash_current_signing_key
 }
 
 resource "railway_variable" "qstash_next_key" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "QSTASH_NEXT_SIGNING_KEY"
   value          = var.qstash_next_signing_key
 }
 
 resource "railway_variable" "qstash_url" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "QSTASH_URL"
   value          = var.qstash_url
 }
 
 # Public URL for the service
 resource "railway_variable" "public_api_url" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "PUBLIC_API_URL"
-  value          = "https://${railway_service.dunner_api.name}-production.up.railway.app" 
+  value          = "https://${railway_service.egida_api.name}-production.up.railway.app" 
 }
 
 resource "railway_variable" "stripe_key" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "STRIPE_SECRET_KEY"
   value          = var.stripe_secret_key
 }
 
 resource "railway_variable" "stripe_webhook_secret" {
-  service_id     = railway_service.dunner_api.id
-  environment_id = railway_project.dunner_project.default_environment.id
+  service_id     = railway_service.egida_api.id
+  environment_id = railway_project.egida_project.default_environment.id
   name           = "STRIPE_WEBHOOK_SECRET"
   value          = var.stripe_webhook_secret
 }
