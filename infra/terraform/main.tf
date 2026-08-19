1a77c5b457d8930463fae4f85f11acf520ce8a78
Santiago Daleffe <santiagodaleffe@gmail.com>
2026-08-19 20:36:00 -0300

    .

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,7 +1,7 @@
 # 1. Create project
 resource "railway_project" "egida_project" {
   name        = "egida"
-  description = "Fucina Egida - Dunning Engine"
+  description = "Egida"
 }
 
 # 2. Create service
@@ -9,6 +9,7 @@ resource "railway_service" "egida_api" {
   project_id      = railway_project.egida_project.id
   name            = "egida-api"
   source_repo     = var.github_repo
+  sleep_application = true
 }
 
 # 3. Inject environment variables
